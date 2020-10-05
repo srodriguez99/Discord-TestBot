07fcbdd1e2b8b15f158031605d7d5e43ac28f74d
srodriguez99 <54964625+srodriguez99@users.noreply.github.com>
2020-10-04 17:59:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Discord = require('discord.js');
- const client = new Discord.Client();
+const client = new Discord.Client();
+const pref = '!';
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
  });
 
 client.on('message', msg => {
+    if(msg.author.bot) return;
+    
     if (msg.content === 'Hello This Is a Test, how are you doing today Bidoof Bot') {
         ///ranodm number bettween 1 & 10
         var random = Math.floor(Math.random() * 10)+1;
@@ -39,10 +42,32 @@ client.on('message', msg => {
         }
         else if(random == 10){
             msg.reply(' Thank you for your comment, I am doing great thanks for asking');
+        } 
+    }  
+    //slice only gets the the ! for pref
+    //trim removes whitespaces
+    //split just splits up word
+    //shift in this case removes the first character
+    else{
+        const tag = msg.content.slice(pref.length).trim().split(" ");
+        const command = tag.shift().toLocaleLowerCase();
+    
+        if (command === 'kick'){
+    
+            const kickUser = msg.mentions.users.first();
+            var uKick = msg.guild.member(kickUser);
+            uKick.kick();
+            msg.reply(`${kickUser.tag} was kicked fromt he server!`);
         }
-        
-        
-    }   
+        else if(command === 'ban'){
+    
+            const discordBan = msg.mentions.users.first();
+            var uBan = msg.guild.member(banUser);
+            uBan.ban();
+            msg.reply(`${banUser.tag} was banned fromt he server!`);
+        }
+    } 
+
  });
 
 client.login('Your Token');
